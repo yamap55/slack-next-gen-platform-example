3f02c477463dbe134cbc597274b5779b2f5ded88
yamap55 <1785817+yamap55@users.noreply.github.com>
2023-01-04 02:27:22 +0900

    [update] notify of channel creation

--- sleepy-frog-160/example.ts
+++ sleepy-frog-160/example.ts
@@ -24,12 +24,33 @@ export const workflow = DefineWorkflow({
   },
 });
 
-workflow.addStep(Schema.slack.functions.SendMessage, {
-  channel_id: workflow.inputs.channel_id,
-  message:
-    `Hi <@${workflow.inputs.creator_id}>, thanks for creating this channel!`,
-});
+const convertStringFromDate = (unixtime) => {
+    console.log(parseInt(unixtime, 10))
+    const date = new Date(parseInt(unixtime, 10) * 1000)
+    const zeroPadding = (s: number) => (`0${s}`).slice(-2)
+    const year_str = date.getFullYear();
+    //月だけ+1すること
+    const month_str = zeroPadding(1 + date.getMonth())
+    const day_str = zeroPadding(date.getDate())
+    const hour_str = zeroPadding(date.getHours())
+    const minute_str = zeroPadding(date.getMinutes())
+    const second_str = zeroPadding(date.getSeconds())
+    return `${year_str}/${month_str}/${day_str} ${hour_str}:${minute_str}:${second_str}`
+};
+const createMessage = (inputs): string => {
+    const creator = `作成者: <@${inputs.creator_id}>`
+    const channel_name = `チャンネル名: #${inputs.channel_name}`
+    const channel_type = `チャンネル種別: ${inputs.channel_type}`
+    // 作成日は「workflow.inputs.created」で文字列のunixtimeが取得可能だが、変換できなかったため割愛
+    // 技術力不足かAPIのバグか
+    // convertStringFromDate(inputs.created)
+    return [creator, channel_name, channel_type].join("\n")
 
+}
+workflow.addStep(Schema.slack.functions.SendMessage, {
+    channel_id: "C04H8294NUB", // 既定のチャンネルに投稿する
+    message: createMessage(workflow.inputs)
+});
 // -------------------------
 // トリガー定義
 // -------------------------
